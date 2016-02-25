5a4782df55ae2e9f2cac0bcfba5938d5fb5129e4
Donald Hanson <don@gridwalk.net>
2016-02-25 14:34:05 -0800

    added livereload for local dev

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     cssnano      = require('gulp-cssnano'),
     autoprefixer = require('gulp-autoprefixer'),
-    rename       = require('gulp-rename')
+    rename       = require('gulp-rename'),
+    livereload   = require('gulp-livereload')
 
 var paths = {
-  styles: ['./scss/**/*']
+  styles: ['./scss/**/*'],
+  php:    ['./**/*.php']
 };
 
 gulp.task('styles', function(){
@@ -18,10 +20,21 @@ gulp.task('styles', function(){
     .pipe(cssnano())
     .pipe(rename('style.css'))
     .pipe(gulp.dest('./'))
+    .pipe(livereload())
+});
+
+gulp.task('livereload-listen', function(){
+  livereload.listen()
+});
+
+gulp.task('reload',  function() {
+  console.log('reloading.....')
+  livereload.reload();
 });
 
 gulp.task('watch', function(){
   gulp.watch(paths.styles, ['styles'])
+  gulp.watch(paths.php, ['reload'])
 })
 
-gulp.task('default', ['styles','watch'])
+gulp.task('default', ['styles','livereload-listen','watch'])
