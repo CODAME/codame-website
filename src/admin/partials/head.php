dacf98bb27ebca39e465f21ae85bc75d3429d5d4
lunaceee <lunaceee@gmail.com>
2020-07-10 13:19:00 -0700

    fix non secure links, add Content-Security-Policy to .htaccess file to auto update non secure links

--- src/admin/partials/head.php
+++ src/admin/partials/head.php
@@ -5,7 +5,7 @@ include('functions.php');
 ?>
 
 <head>
-  <link href='http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="../style.css">
   <? include('login.php'); ?>
 </head>
